444b0cd2b7a374874b331242e1cb543d61f61bcd
Pluto Yang <yangyj.ee@gmail.com>
2023-06-19 19:25:26 +0800

    和 chatglm-web 接口兼容

--- api_proxy.py
+++ api_proxy.py
@@ -1,6 +1,6 @@
 import time
 from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 from UltraDict import UltraDict
 import json, uuid
 import requests
@@ -11,13 +11,13 @@ status = UltraDict(name='fastapi_dict')
 server_list = []
 
 # 初始化服务器列表
-def init_dict():
+def init_list():
     with open('servers.txt', 'r') as f:
         for line in f:
             line = line.strip()
             server_list.append(line)
 
-init_dict()
+init_list()
 with status.lock:
     if not status.has_key('init'):
         status['init'] = True # 保证初始化一次
@@ -28,32 +28,60 @@ with status.lock:
 app = FastAPI()
 headers = {"Content-Type": "application/json"}
 payload = {"prompt": "",
-           "temperature": 0.2,
+           "temperature": 0.4,
            "history":[]}
+
 # 生成数据的生成器函数
-async def generate_data(index):
+async def generate_data(index, options):
+    uid = None
+    if options:
+        uid = options.get("parentMessageId")
+        with status.lock:
+            if status.has_key(uid):
+                payload['history'] = status[uid]
+            else:
+                payload['history'] = []
+    else:
+        payload['history'] = []
+        uid = uuid.uuid4().hex
     url = 'http://' + server_list[index]
+    print(payload)
     response = requests.post(url, json=payload, headers=headers, stream=True)
+    last_line = ''
     for line in response.iter_content(chunk_size=None):
         idx = line.rfind(b'<^>')
+        last_line = line[idx+3:].decode('utf-8')
         message = json.dumps(dict(
             role = "AI",
-            id = "chatglm",
-            parentMessageId = None,
-            text = line[idx+3:].decode('utf-8'),
+            id = uid,
+            text = last_line,
         ))
         yield "<^>" + message
     with status.lock:
         status[str(index)] = False
         status['ready'] += 1
+        payload['history'].append([payload['prompt'], last_line])
+        status[uid] = payload['history']
+
+@app.post("/config")
+async def config():
+    return JSONResponse(content=dict(
+        message=None,
+        status="Success",
+        data=dict()
+    ))
 
 @app.get("/status")
 async def get_status():
-    return {"ready", status['ready']}
+    return {"ready": status['ready']}
 
 @app.post("/")
 async def stream_data(data: dict):
     idx = 0
+    options = data['options']
+    print(data)
+    payload['prompt'] = data['prompt']
+    payload['temperature'] = data['temperature']
     while True:
         if idx == len(server_list):
             idx = 0
@@ -65,7 +93,7 @@ async def stream_data(data: dict):
                 break;
         idx += 1
     print(f"Using {idx}\n")
-    return StreamingResponse(generate_data(idx), media_type="text/plain")
+    return StreamingResponse(generate_data(idx, options), media_type="text/plain")
 
 if __name__ == "__main__":
     import uvicorn
